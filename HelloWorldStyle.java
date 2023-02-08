9f9a1005051d6776b4821dcdd3e436414e8e20ea
Alexander Matheson <alexander.matheson@stu.ocsb.ca>
2023-02-08 18:50:04 +0000

    Finally worked out the style checker

---
+++ HelloWorldStyle.java
@@ -0,0 +1,20 @@
+/**
+* This program prints several messages.
+*
+* @author  Alexander Matheson
+* @version 1.0
+* @since   2023-02-07
+*/
+
+public final class HelloWorldStyle {
+    /**
+    * Print messages.
+    *
+    * @param args Unused
+    */
+    public static void main(String[] args) {
+        System.out.print("Next year, I intend to study");
+        System.out.println(" Computer Sciences at Mount Allison University.");
+        System.out.println("I feel excited for it.");
+    }
+}
